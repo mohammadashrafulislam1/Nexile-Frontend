d74d5466398fe03c91d05d2d43db5669c5213a4d
Md Ashraful Islam <90489175+mohammadashrafulislam1@users.noreply.github.com>
2026-07-04 12:49:30 -0600

    Fix import statement for index.css

--- src/pages/LandingPage/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage/LandingPage.jsx
@@ -12,7 +12,7 @@ import FAQs from "./FAQs";
 import ContactBtn from "./ContactBtn";
 import Blogs from "./Blogs";
 import Footer from "../../../Components/ForAll/Footer";
-import ""../../../index.css"
+import "../../../index.css"
 
 const LandingPage = () => {
   const [locationData, setLocationData] = useState(null);
